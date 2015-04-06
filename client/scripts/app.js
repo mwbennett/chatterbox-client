101fb03f282607e2838959ef9ddffdc93b5dee86
mwbennett <bennettwmark@gmail.com>
2015-04-06 15:06:41 -0700

    Added messages to page

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,32 @@
 // YOUR CODE HERE:
-
+$(function(){
+  $.ajax({
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  contentType: 'application/json',
+  success: function (data) {
+    // debugger;
+    if (data.results){
+      _.each(data.results, function(message){
+        var $wrapper = $('<div></div>');
+        $wrapper.addClass('messageWrapper');
+        var $username = $('<div></div>');
+        $username.html(message.username);
+        $username.addClass('username');
+        $username.appendTo($wrapper);
+        var $text = $('<div></div>');
+        $text.html(message.text);
+        $text.addClass('messageText');
+        $text.appendTo($wrapper);
+        $('#main').append($wrapper);
+      });
+    }
+    console.log(data);
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+  });
+});
